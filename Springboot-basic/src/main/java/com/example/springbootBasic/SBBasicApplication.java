a66552f6158a845f09d9c41a1c68adfc497b69a2
yichenkang <1246263205@qq.com>
2022-03-15 23:26:08 +0800

    test method

--- Springboot-basic/src/main/java/com/example/springbootBasic/SBBasicApplication.java
+++ Springboot-basic/src/main/java/com/example/springbootBasic/SBBasicApplication.java
@@ -2,10 +2,6 @@ package com.example.springbootBasic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.core.env.Environment;
-
-import java.net.InetAddress;
 
 /**
  * @author 小易
@@ -15,16 +11,17 @@ import java.net.InetAddress;
 @SpringBootApplication
 public class SBBasicApplication {
     public static void main(String[] args) {
-        ConfigurableApplicationContext context = SpringApplication.run(SBBasicApplication.class, args);
-        String ip = null;
-        InetAddress ia = null;
-        try {
-            ia = ia.getLocalHost();
-            ip = ia.getHostAddress();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        Environment environment = context.getBean(Environment.class);
+        SpringApplication.run(SBBasicApplication.class, args);
+//        ConfigurableApplicationContext context = SpringApplication.run(SBBasicApplication.class, args);
+//        String ip = null;
+//        InetAddress ia = null;
+//        try {
+//            ia = ia.getLocalHost();
+//            ip = ia.getHostAddress();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        Environment environment = context.getBean(Environment.class);
 
 //        System.err.println("doc:http://" + ip + ":" + environment.getProperty("local.server.port") + "/doc.html");
 //        System.err.println("swagger:http://" + ip + ":" + environment.getProperty("local.server.port") + "/swagger-ui.html");
--- Springboot-basic/src/main/java/com/example/springbootBasic/controller/TestController.java
+++ Springboot-basic/src/main/java/com/example/springbootBasic/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.example.springbootBasic.controller;
 
 import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -13,6 +14,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
 
-
+    @GetMapping("/test")
+    public String test() {
+        return "hello";
+    }
 
 }
